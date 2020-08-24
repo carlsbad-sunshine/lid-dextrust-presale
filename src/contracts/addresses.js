7bd86d3f8666a854894f794610c11cc47b8b6369
carlsbad <carlsbad_sunshine@protonmail.com>
2020-08-24 00:43:02 -0700

    Update contract information

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -1,9 +1,7 @@
 const addresses = {
-  presale: '',
-  daofund: '',
-  teamfund: '',
-  redeemer: '',
-  timer: '',
+  presale: '0x9bf9bEC123964291A793585bcfbec6cA7a85d304',
+  redeemer: '0x9DD39336d9a2aAf44B58b785a12cc22E51eC87B1',
+  timer: '0x3AF773DA7bD20332a2E6e07C76e8Edb4EDb7Bd3d',
   token: '0xd379700999f4805ce80aa32db46a94df64561108'
 };
 
